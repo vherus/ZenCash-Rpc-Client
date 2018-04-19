43953782cfaf78eb6c081c4591e6c48c69b77690
Nathan King <vherus@users.noreply.github.com>
2018-04-19 15:39:59 +0100

    Make Help arg optional

--- src/Command/Control/Help.php
+++ src/Command/Control/Help.php
@@ -9,18 +9,19 @@ final class Help implements Command
     private const METHOD = 'help';
     private $command;
 
-    public function __construct(string $command)
+    public function __construct(?string $command)
     {
         $this->command = $command;
     }
 
     public function jsonSerialize(): object
     {
-        return (object) [
+        return (object) array_merge([
             'jsonrpc' => Command::JSON_RPC_VERSION,
             'id'      => Command::ID,
-            'method'  => self::METHOD,
+            'method'  => self::METHOD
+        ], !is_null($this->command) ? [
             'params' => [ $this->command ]
-        ];
+        ] : [ ]);
     }
 }
--- tests/Command/Control/HelpTest.php
+++ tests/Command/Control/HelpTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 class HelpTest extends TestCase
 {
-    public function test_serialize_returns_expected_object()
+    public function test_serialize_with_param_returns_expected_object()
     {
         $expected = (object) [
             'jsonrpc' => '1.0',
@@ -16,5 +16,16 @@ class HelpTest extends TestCase
         ];
 
         $this->assertEquals($expected, (new Help('getinfo'))->jsonSerialize());
+    }
+
+    public function test_serialize_without_param_returns_expected_object()
+    {
+        $expected = (object) [
+            'jsonrpc' => '1.0',
+            'id'      => 'curl',
+            'method'  => 'help'
+        ];
+
+        $this->assertEquals($expected, (new Help(null))->jsonSerialize());
     }
 }
