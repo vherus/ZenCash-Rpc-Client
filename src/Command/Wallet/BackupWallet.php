78812f0dd03ad3c52e1057dfbba15596705f1857
Nathan King <vherus@users.noreply.github.com>
2018-04-20 15:40:59 +0100

    Add backupwallet command

---
+++ src/Command/Wallet/BackupWallet.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ZenCash\Rpc\Command\Wallet;
+
+use ZenCash\Rpc\Command;
+
+final class BackupWallet implements Command
+{
+    private const METHOD = 'backupwallet';
+    private $filename;
+
+    public function __construct(string $filename)
+    {
+        $this->filename = $filename;
+    }
+
+    public function jsonSerialize(): object
+    {
+        return (object) [
+            'jsonrpc' => Command::JSON_RPC_VERSION,
+            'id'      => Command::ID,
+            'method'  => self::METHOD,
+            'params' => [ $this->filename ]
+        ];
+    }
+}
